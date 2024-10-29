265fc800bfccd8bd6826b1a6cce7754f2c2e1557
20373-1207 <olivia11@go.edu.tw>
2024-10-29 20:01:39 +0800

    Create ex3-3.py

--- ex3-3.py
+++ ex3-3.py
@@ -1,1 +1,20 @@
+'''
 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#x = int(1)    #x will be 1
+#y = int(2.8)    #y will be 2
+#z = int("3")    #z will be 3
+x=1    #x will be 1
+y=2.8    #y will be 2
+z="3"    #z will be 3
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
