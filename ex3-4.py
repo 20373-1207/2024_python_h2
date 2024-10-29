0833a397a3777e095f1f1146f34d98d507a1e60d
20373-1207 <olivia11@go.edu.tw>
2024-10-29 20:46:58 +0800

    Create ex3-4.py

--- ex3-4.py
+++ ex3-4.py
@@ -19,3 +19,27 @@ print(f'Hello World x = {x:.5f}')
 
 x=int(input("Enter int x:"))
 print(f'Hello World x ={x:5d}')
+
+----------------------------------------------------------------------------------------------------------------
+
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+x=3.14159
+y=1.123
+print(f'Hello World x={x:.5f}' ,x)
+
+username = input("Enter Username:")
+
+print("Username is: "+username)
+#x=float(input("Enter float x:"))
+x=float(input("Enter float x:"))
+print(f'Hello World x = {x:.5f}')
+
+x=int(input("Enter int x:"))
+print(f'Hello World x ={x:5d}')
