b6fe2669647b351c4fbf5fada0595ab19e2ba104
20373-1207 <olivia11@go.edu.tw>
2024-11-05 20:31:11 +0800

    Create ex4-9.py

---
+++ ex4-9.py
@@ -0,0 +1,10 @@
+print("please input a:")
+a=int(input())
+print("please input b:")
+b=int(input())
+if b<a:
+  print("b is smaller than a ")
+elif b>a:
+  print("b is greater than a ")
+else:
+  print("b is equal to a ")
