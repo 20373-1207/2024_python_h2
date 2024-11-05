19882feb20c9f47a9d41b3fa3de36674237166c0
20373-1207 <olivia11@go.edu.tw>
2024-11-05 20:14:55 +0800

    Create ex4-7.py

---
+++ ex4-7.py
@@ -0,0 +1,34 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  
+a = 33
+b = 200
+if b==a:
+  print("b is greater than a")
+  
+a = 33
+b = 200
+if b < a:
+  print("b is greater than a")
+  
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  
+a = 33
+b = 200
+if b >= a:
+  print("b is greater than a")
+  
+a = 33
+b = 200
+if b <= a:
+  print("b is greater than a")
+  
+a = 33
+b = 200
+if b != a:
+  print("b is greater than a")
