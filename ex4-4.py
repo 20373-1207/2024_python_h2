68bd6d0ae8a9e41fba453d7f4eb8e80ef4553a91
20373-1207 <olivia11@go.edu.tw>
2024-11-05 19:42:59 +0800

    Create ex4-4.py

---
+++ ex4-4.py
@@ -0,0 +1,15 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+F-String 是在 Python 3.6 中引入的，現在是格式化字串的首選方式。
+
+要將字串指定為 f 字串，只需f在字串文字前面放置一個，並添加大括號{}作為變數和其他運算的佔位符。
