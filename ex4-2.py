d73eafe152229edfd5681b470e7227041fc6a439
20373-1207 <olivia11@go.edu.tw>
2024-11-05 19:39:57 +0800

    Create ex4-2.py

---
+++ ex4-2.py
@@ -0,0 +1,12 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+a = "Hello, World!"
+print(a.upper())
+此upper()方法傳回大寫的字串
+
