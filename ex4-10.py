015a6da0f89e6ac3da56b2e8e59121855e546313
20373-1207 <olivia11@go.edu.tw>
2024-11-05 20:38:46 +0800

    Create ex4-10.py

---
+++ ex4-10.py
@@ -0,0 +1,13 @@
+AND用法:
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+OR用法:
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
