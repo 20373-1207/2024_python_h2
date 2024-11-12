c127ea7b0cb60dfe279529a0b03b20e85f34f371
20373-1207 <olivia11@go.edu.tw>
2024-11-12 19:24:45 +0800

    Create ex5-1.py

--- ex5-1.py
+++ ex5-1.py
@@ -1,3 +1,4 @@
+break --> 強制停止程式
 使用break語句，即使while條件為真，我們也可以停止迴圈：
 當 i 為 3 時退出循環：
 i = 1
@@ -10,4 +11,26 @@ while i < 6:
 +=:加法和賦值
 　　　　將等號左邊指定為等號左邊加上等號右邊，i+=1代表i = i + 1
 
-break --> 強制停止程式
+continue-->持續執行程式
+使用continue語句，我們可以停止目前迭代，並繼續下一個迭代：
+如果 i 為 3，則繼續下一個迭代：
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+
+else
+使用else語句，我們可以在條件不再為真時執行一次程式碼區塊：
+一旦條件為假，列印一則訊息：
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
