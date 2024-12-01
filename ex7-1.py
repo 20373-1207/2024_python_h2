cd1400bc25561bd72766378b99f392e71ab7922b
20373-1207 <olivia11@go.edu.tw>
2024-12-01 22:01:30 +0800

    Create ex7-1.py

---
+++ ex7-1.py
@@ -0,0 +1,30 @@
+Array
+數組
+  
+新增數組元素
+您可以使用該append()方法將元素新增至陣列。
+在數組中再加入一個元素cars：
+cars.append("Honda")
+
+刪除數組元素
+您可以使用該pop()方法從陣列中刪除元素
+刪除陣列的第二個元素cars：
+cars.pop(1)
+您也可以使用該remove()方法從陣列中刪除元素。
+刪除值為「Volvo」的元素：
+cars.remove("Volvo")
+
+數組方法
+Python 有一組可以在列表/陣列上使用的內建方法。
+
+append()	Adds an element at the end of the list
+clear()	Removes all the elements from the list
+copy()	Returns a copy of the list
+count()	Returns the number of elements with the specified value
+extend()	Add the elements of a list (or any iterable), to the end of the current list
+index()	Returns the index of the first element with the specified value
+insert()	Adds an element at the specified position
+pop()	Removes the element at the specified position
+remove()	Removes the first item with the specified value
+reverse()	Reverses the order of the list
+sort()	Sorts the list
