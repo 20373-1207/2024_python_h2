488b38acda46d8e5a464eae0cfac6f4a29de0ac8
20373-1207 <olivia11@go.edu.tw>
2024-12-01 22:41:57 +0800

    Create ex7-3.py

---
+++ ex7-3.py
@@ -0,0 +1,12 @@
+x=int(input("Enter int x:"))
+print(f'Hello World x = {x:5d})
+
+y=int(input("Enter int y:"))
+print(f'Hello World y = {y:5d})
+
+if x > y:
+  print("x > y")
+elif x<y:
+  print("x < y")
+else:
+  print("x == y")
