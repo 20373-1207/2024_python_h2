402c3eb160c6cc330228adca318dadd3937faf38
20373-1207 <olivia11@go.edu.tw>
2024-12-01 22:08:12 +0800

    Create ex7-2.py

---
+++ ex7-2.py
@@ -0,0 +1,15 @@
+cars = ["Ford","Volvo","BMW"]
+print(cars)
+print(cars[0])
+print(cars[1])
+print(cars[2])
+for x in cars:
+  print(x)
+
+
+x=3*[0]
+
+for i in range(0,3):
+  x[i] = int(input())
+# print("i= ",x[i])
+print(x)
