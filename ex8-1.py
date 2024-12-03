555941f0217ec047e72e440956b3d2826e6d1f46
20373-1207 <olivia11@go.edu.tw>
2024-12-03 20:56:38 +0800

    Create ex8-1.py

---
+++ ex8-1.py
@@ -0,0 +1,19 @@
+thislist=["apple","banana","cherry"]
+print(thislist)
+for x in thislist:
+  print(x)
+print(len(thislist))
+
+
+for x in range(0,len(thislist))
+  print(X,"  ")
+  print(thislist[x])
+thislist[0]="hello"
+print(thislist)
+
+hw:
+13、程式設計序實現求1-1000之間的所有奇數的和並輸出。
+
+21. 主函式輸入 輸入 a 小時 b 分， 其中自定義一函式，時間換算，輸出  x 分
+
+30. 用陣列實現以下功能：輸入5個學生成績，而後求出這些成績的平均值並顯示出來。
