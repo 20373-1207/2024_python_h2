0fe8e42b897f2816dbc161ca0e761899ab0ae83a
20373-1207 <olivia11@go.edu.tw>
2024-12-10 20:30:15 +0800

    Create ex9-3.py

---
+++ ex9-3.py
@@ -0,0 +1,13 @@
+python Dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print(len(thisdist))
+print(thisdict["brand"])
+print(thisdict["model"])
+print(thisdict["year"])
+
