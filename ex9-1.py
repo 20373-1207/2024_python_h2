7b618c50a2798416ae266e9c07007fad7c781609
20373-1207 <olivia11@go.edu.tw>
2024-12-10 20:11:42 +0800

    Create ex9-1.py

---
+++ ex9-1.py
@@ -0,0 +1,27 @@
+錯誤:
+thist =(“apple”, “banana”, “cherry”)
+print(thist)
+for x in thist:
+	print(x)
+print(len(thist))
+for x in range(0,len(thist))
+	print(x, “             ”)
+	print(thist[x])
+thist[0]= “hello”
+print(thist)
+
+正確:
+thist= {"apple", "banana", "cherry"}
+print(thist)
+for x in thislist:
+	print(x)
+print(len(thist))
+
+for x in range(0,len(thislist)):
+	print(x,"")
+	print(thist[x])
+
+thislist[0]= "hello"
+print(thislist)
+
+
